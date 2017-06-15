8759cf318cbb5b74184d0495e9d110e2579f9e97
danieltbrad <danieltbrad@gmail.com>
2017-06-14 17:30:42 -0700

    enable CORS

--- Gemfile
+++ Gemfile
@@ -20,6 +20,7 @@ gem 'uglifier', '>= 1.3.0'
 # gem 'therubyracer', platforms: :ruby
 gem 'nokogiri', '~> 1.6', '>= 1.6.8'
 gem 'active_model_serializers', '~> 0.10.6'
+gem 'rack-cors'
 
 # Use CoffeeScript for .coffee assets and views
 gem 'coffee-rails', '~> 4.2'
--- config/application.rb
+++ config/application.rb
@@ -8,11 +8,22 @@ Bundler.require(*Rails.groups)
 
 module SeriousRecipesRails
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.1
-
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    # Only loads a smaller set of middleware suitable for API only apps.
+    # Middleware like session, flash, cookies can be added back manually.
+    # Skip views, helpers and assets when generating a new resource.
+    config.api_only = true
+    # config.autoload_paths << Rails.root.join('lib')
+
+    config.middleware.insert_before 0, Rack::Cors do
+     allow do
+       origins '*'
+       resource '*', :expose => ['jwt', 'item_count'], :headers => :any, :methods => [:get, :post, :options]
+     end
+   end
+
   end
 end
